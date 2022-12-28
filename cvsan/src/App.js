583437c89acfda02a35740fba8ba9d0eb4439d6e
san896 <sannlfernandez8@gmail.com>
2022-12-27 21:39:00 -0300

    yendo

--- cvsan/src/App.js
+++ cvsan/src/App.js
@@ -1,41 +1,45 @@
 //import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
 
 function App() {
   return (
-    <div >
-      <header >
+    <div className = {style.div}>
+      <header className = {style.head} >
         <h1> Santiago Fernandez </h1>
-        <h2> Full Stack Developer </h2>
+        <h2 className = {style.sub}> Full Stack Developer </h2>
       </header>
       
-      <div>
+      <div className={style.contact}>
           <h5>  sannlfernandez8@gmail.com</h5>
           <h5>Cel. +54 1156337228</h5>
           <h5> Buenos Aires - Argentina. </h5>
       </div>
 
-      <div>
+      <div className={style.contact}>
         <h5> linkedin</h5>
         <h5> portfolio</h5>
         <h5> github </h5>
       </div>
 
+
+      <div className = {style.p}>
       <p>
       Full Stack Developer con formación como Desarrollador Web. Experiencia trabajando en JS, NodeJS, Express, React, Redux, SQL, Sequelize, Scrum entre otras tecnologías del sector.<br/>
 Trabaje en conjunto con mi equipo para crear un SPA(single page app), orientada a una instituion deportiva/academica con un e-comerce integrado, posee una sección de manejo y administración de usuarios entre muchas mas funcionalidades, mi aporte fue bastatnte completo tanto en base de datos como back y front.<br/>
 Con pensamiento creativo, empatia, trabajo en equipo, autonomia y buena resolucion de problemas. 
       </p>
+      </div>
 
-      <h4> TECNOLOGIAS </h4>
-      <p>   SQL, PostgreSql, Sequelize, Node.js, Express, JavaScript, HTML, CSS, Bootstrap, React, Redux, Scrum, Git, Cloudinary.    </p>
 
-      <h4> PROYECTOS </h4>
+      <h4 className={style.titles}> TECNOLOGIAS: </h4>
+      <p className = {style.p}>   SQL | PostgreSql | Sequelize | Node.js | Express | JavaScript | HTML | CSS | Bootstrap | React | Redux | Scrum | Git | Cloudinary.    </p>
+
+      <h4 className={style.titles}> PROYECTOS: </h4>
       <div>
-      <h5> Full Stack Web Developer – “AthenasClub App” </h5>
-      <h6> Henry Bootcamp, Buenos Aires, Argentina</h6>
-      <h6> Oct 2022 – Nov 2022 </h6>
-      <ul>
+      <h5 className={style.titles}> Full Stack Web Developer – “AthenasClub App” </h5>
+      <h6 className={style.titles}> Henry Bootcamp, Buenos Aires, Argentina</h6>
+      <h6 className={style.titles}> Oct 2022 – Nov 2022 </h6>
+      <ul >
         <li>Diseñar y desarrollar una Single Page Aplication(SPA),  orientada a una institucion deportiva, con  el objetivo de crear una buena UI y UX. Cuenta con un e-commerce integrado, pasarela de pagos de mercado pago, tambien con autenticacion de usuarios con Auth0, incluye un dashboard para el administrador y usuarios. Creacion y organizacion de productos, imagenes subidas a Cloudinary, reviews de los productos entre muchas mas funcionalidades. </li>
         <li>Tecnologias usadas en la SPA: Front React, Redux, Bootstrap y Back desarrollado en Node.js con Express. Base de datos en PostgreSQL y Sequelize. </li>
         <li> linkedin</li>
@@ -45,9 +49,9 @@ Con pensamiento creativo, empatia, trabajo en equipo, autonomia y buena resoluci
 
 
       <div>
-      <h5> Full Stack Web Developer – “Food App” </h5>
-      <h6> Henry Bootcamp, Buenos Aires, Argentina</h6>
-      <h6> Sept. 2022 - Oct. 2022  </h6>
+      <h5 className={style.titles}> Full Stack Web Developer – “Food App” </h5>
+      <h6 className={style.titles}> Henry Bootcamp, Buenos Aires, Argentina</h6>
+      <h6 className={style.titles}> Sept. 2022 - Oct. 2022  </h6>
       <ul>
         <li>Diseñar y desarrollar una App sobre Recetas de comida, las cual  incluye búsquedas, filtrados, ordenamientos por tipos de receta, nivel de salud, nombre, entre otras. Tambien cuenta con un formulario controlado para la creacion de recetas. Fue creada utilzando una APi externa.  </li>
         <li>Desarrollar la app usando para el Front React, Redux, CSS puro y Back desarrollado en Node.js con Express. Base de datos en PostgreSQL y Sequelize.  </li>
@@ -56,13 +60,13 @@ Con pensamiento creativo, empatia, trabajo en equipo, autonomia y buena resoluci
       </ul>
       </div>
 
-      <h4> EDUCACIÓN PROFESIONAL  </h4>
+      <h4  className={style.titles}> EDUCACIÓN PROFESIONAL  </h4>
       <ul>
         <li> Full Stack Web Developer. Henry Bootcamp. +800 horas de cursado teórico-práctico. 2021-2022. </li>
         <li> 3 años de carrea en Ingenieria Electromecanica- UNGS(Universidad Nacional de Gral Sarmiento) </li>
       </ul>4
 
-      <h4>EXPERIENCIA PROFESIONAL/OTRAS EXPERIENCIAS</h4>
+      <h4 className={style.titles}>EXPERIENCIA PROFESIONAL/OTRAS EXPERIENCIAS</h4>
       <ul>
         <h5>Productor Asesor de Seguros – RF Asesores/Productores de seguros </h5>
         <h6> 2014 – 2022(actualidad)    </h6>
@@ -73,19 +77,19 @@ Con pensamiento creativo, empatia, trabajo en equipo, autonomia y buena resoluci
         <li>Emprendimiento, venta de baterias para vehiculos, testeos, asesoramientos</li>
       </ul>
 
-      <h4> EDUCACIÓN COMPLEMENTARIA  </h4>
+      <h4  className={style.titles}> EDUCACIÓN COMPLEMENTARIA  </h4>
       <ul>
         <li> Escuela de Javascript. Platzi. 2021. </li>
       </ul>
 
 
-      <h4> IDIOMAS </h4>
+      <h4  className={style.titles}> IDIOMAS </h4>
       <ul>
         <li>  Inglés - B2 Upper Intermediate(EFSET) </li>
         <li>  Portugues - Basic </li>
       </ul>
 
-      <h4> COMPETENCIAS </h4>
+      <h4  className={style.titles}> COMPETENCIAS </h4>
       <ul>
         <li> Creatividad </li>
         <li> Trabajo en equipo </li>
